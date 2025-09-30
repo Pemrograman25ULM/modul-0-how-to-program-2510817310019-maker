c170b184b164a83978e5693165502e4517826c29
BLACK1 <2510817310019@mhs.ulm.ac.id>
2025-09-30 12:11:40 +0800

    Add files via upload

---
+++ C/c/PRAK001-2510817310019-MUHAMMAD HAMDANI.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main(){
+    printf("saya calon programmer no 1");
+}
---
+++ C/c/PRAK004-2510817310019- MUHAMMAD HAMDANI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#      MUHAMMAD HAMDANI      #\n");
+    printf("#        2510817310019       #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    
+    return 0;
+}
